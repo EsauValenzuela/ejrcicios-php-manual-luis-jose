460fcff83ea64cb22805c9130a351173350626cc
Esau Valenzuela Asensio <esauval@gmail.com>
2016-09-23 10:39:03 +0200

    subida ejercicios php

---
+++ ejercicios_tema_1/ejericicio3_tema1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo"<table border = 1 align = center>
+           <tr><td>INGLES</td><td>ESPAÑOL</td></tr>
+           <tr><td>car</td><td>coche</td></tr>
+           <tr><td>dog</td><td>perro</td></tr>
+           <tr><td>boy</td><td>chico</td></tr>
+           <tr><td>girl</td><td>chica</td></tr>
+           <tr><td>pen</td><td>lapiz</td></tr>
+           <tr><td>body</td><td>cuerpo</td></tr>
+           <tr><td>hair</td><td>pelo</td></tr>
+           <tr><td>rules</td><td>regla</td></tr>
+           <tr><td>home</td><td>casa</td></tr>
+           <tr><td>number</td><td>numero</td></tr>
+            </table>";
+        ?>
+    </body>
+</html>
